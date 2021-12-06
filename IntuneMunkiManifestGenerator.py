a03c0d34904086fe349e105592317bd956c2bbf8
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2021-12-06 18:22:51 +0100

    Update IntuneMunkiManifestGenerator.py
    Version 1.3: 2021-12-06 - Added check for null AAD device ID, removal of department manifest if device is no longer in group and
    removal of manifest if device is no longer in Intune. If you want to do this, un-comment line 300. Run one time first and check output 
    to make sure manifests that should not be deleted are touched.

--- IntuneMunkiManifestGenerator.py
+++ IntuneMunkiManifestGenerator.py
@@ -43,6 +43,10 @@ More info:
 If want to see the setup step by step, please see this blog post:
 https://almenscorner.io/munki-what-about-manifests/
 Release notes:
+Version 1.3: 2021-12-06 - Added check for null AAD device ID, removal of department manifest if device is no longer in group and
+removal of manifest if device is no longer in Intune. If you want to do this, un-comment line 300. Run one time first and check output 
+to make sure manifests that should not be deleted are touched.
+Version 1.2: 2021-10-04 - Improvements in department group handling and clearer output per device.
 Version 1.1: 2021-09-28 - The script now checks if included manifests are missing for already created device manifests and adds them.
 Version 1.0: 2021-09-24 - Original published version.
 Many thanks to journeyofthegeek.com and Shashank Mishra for many of the defs and jbaker10 for the original idea of manifest generation.
@@ -149,29 +153,36 @@ def create_plist_blob(local_file_name,connection_instance,container_name,manifes
 #Get groups the device is a member of
 
 def get_device_memberOf(azureADDeviceId):
+    aad_device_objId = None
     q_param_device = {"$filter":"deviceId eq " + "'" + azureADDeviceId + "'"}
     device_object = makeapirequest(device_group_endpoint,token,q_param_device)
     for id in device_object['value']:
         objId = id['id']
         aad_device_objId = objId
-    q_param_group= {"$select":"id"}
-    memberOf = makeapirequest(device_group_endpoint + "/" + aad_device_objId + "/memberOf",token,q_param_group)
-    device_groups = []
-    for group_id in memberOf['value']:
-        id = group_id['id']
-        device_groups.append(id)
-    for k in department_groups.keys():
-        values = department_groups.get(k)
-        if values['id'] in device_groups:
-            if values['name'] in manifest_dict:
-                print("Device found in group for " + values['name'] + ", adding included manifest for department")
-                device['manifest_list'].append(values['name'])
-            else:
-                print("Device found in group for " + (values['name']) + " but manifest does not exist, skipping")
+    q_param_group= {"$select":"id,displayName"}
+    if aad_device_objId is None:
+        print("AAD Device ID is null, skipping device group memberships")
+    else:
+        memberOf = makeapirequest(device_group_endpoint + "/" + aad_device_objId + "/memberOf",token,q_param_group)
+        device_groups = []
+        device_groups_name = []
+        for group_id in memberOf['value']:
+            id = group_id['id']
+            device_groups.append(id)
+            device_groups_name.append(group_id['displayName'])
+        for k in department_groups.keys():
+            values = department_groups.get(k)
+            if values['id'] in device_groups:
+                if values['name'] in manifest_dict:
+                    print("Device found in group for " + values['name'] + ", adding included manifest for department")
+                    device['manifest_list'].append(values['name'])
+                else:
+                    print("Device found in group for " + (values['name']) + " but manifest does not exist, skipping")
+        return device_groups_name
 
 #Check if device is missing included manifests and add them if any are missing
 
-def update_plist_blob(remote_file_name,connection_instance,container_name,manifest_template,device):
+def update_plist_blob(remote_file_name,connection_instance,container_name,manifest_template,device,member_of):
     try:
         local_path = "./"
         download_file_path = os.path.join(local_path, remote_file_name)
@@ -182,6 +193,7 @@ def update_plist_blob(remote_file_name,connection_instance,container_name,manife
             data = blob_data.readall()
             plist_data = plistlib.loads(data)
             add_manifests=[]
+            remove_manifests=[]
             for manifest in device['manifest_list']:
                 if manifest not in plist_data['included_manifests']:
                     add_manifests.append(manifest)
@@ -189,6 +201,13 @@ def update_plist_blob(remote_file_name,connection_instance,container_name,manife
                     print("Manifest " + manifest + " not found, skipping")
                     device['manifest_list'].remove(manifest)
                     add_manifests.remove(manifest)
+            for group_manifest in plist_data['included_manifests']:
+                if ((group_manifest not in member_of) and (group_manifest != "site_default")):
+                    remove_manifests.append(group_manifest)
+            if remove_manifests:
+                for manifest in remove_manifests:
+                    plist_data['included_manifests'].remove(manifest)
+                    device['manifest_list'] = plist_data['included_manifests']
             plistlib.dump(manifest_template, _f)
 
         if add_manifests:
@@ -196,12 +215,28 @@ def update_plist_blob(remote_file_name,connection_instance,container_name,manife
             with open(download_file_path, "rb") as data:
                 blob_client.upload_blob(data, overwrite=True)
             os.remove(download_file_path)
+        if remove_manifests:
+            print("Device is no longer part of " + manifest + ", removing included manifest")         
+            with open(download_file_path, "rb") as data:
+                blob_client.upload_blob(data, overwrite=True)
+            os.remove(download_file_path)
 
+    except Exception as ex:
+        print ("Error: " + str(ex))
+
+#Delete manifest
+
+def delete_blob(remote_file_name,container_name):
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
+        blob_client = blob_service_client.get_blob_client(container=container_name + "/manifests", blob=remote_file_name)
+        blob_client.delete_blob()
     except Exception as ex:
         print ("Error: " + str(ex))
 
 #Create dicts and objects
 devices = []
+serials = []
 manifest_dict = {}
 catalogs = ["Production"]
 department_groups = {
@@ -251,10 +286,23 @@ for i in range(0, len(data['value'])):
 for manifest in current_manifests:
     manifest_dict[manifest] = 1
 
+for sn in devices:
+    serials.append(sn['serialNumber'])
+
+#If manifest is not in device serials or department groups and is not site_default, remove manifest
+for manifest in manifest_dict:
+    groups = []
+    for k in department_groups.keys():
+        values = department_groups.get(k)
+        groups.append(values['name'])
+    if ((manifest not in serials) and (manifest not in groups) and (manifest != "site_default")):
+        print('Device ' + manifest + ' not found, deleting device manifest')
+        #delete_blob(manifest,container_name)
+
 for device in devices:
     if device['serialNumber'] in manifest_dict:
         print("-" * 42 + device['serialNumber'] +  "-" * 42)
-        get_device_memberOf(device['id'])
+        member_of = get_device_memberOf(device['id'])
         manifest_template = {}
         manifest_template['catalogs'] = catalogs
         for name in device['manifest_list']:
@@ -268,7 +316,7 @@ for device in devices:
         manifest_template['serialnumber'] = device['serialNumber']
         manifest_template['user'] = device['user']
         print("Manifest already exists, skipping manifest creation")
-        update_plist_blob(device['serialNumber'],connection_instance,container_name,manifest_template,device)
+        update_plist_blob(device['serialNumber'],connection_instance,container_name,manifest_template,device,member_of)
         print("-" * 96)
     else:
         print("-" * 42 + device['serialNumber'] +  "-" * 42)
